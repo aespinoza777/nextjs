e439cb45a91a42d18bf78ca43a47523a91c40ddb
AntonioEsp <github email address>
2022-06-09 20:19:19 -0500

    moved menu file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Menu from '../pages/api/Menu'
+import Menu from '../.next/components/Menu'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
